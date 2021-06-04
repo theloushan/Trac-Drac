31dfa4ddd0de275abae23336e1f3f08845c491ef
Cidor_Galtorn <84996208+Galtorn@users.noreply.github.com>
2021-06-04 18:13:10 -0400

    Animation Switches according to y position of mouse

--- DraculaRunner/charSprite.gd
+++ DraculaRunner/charSprite.gd
@@ -2,17 +2,28 @@ extends AnimatedSprite
 
 # Declare member variables here. Examples:
 onready var _animated_sprite = get_node('../charSprite')
- 
+var act_pos
+var charS_act
+var t = 0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	var mouse_pos = get_global_mouse_position()
-	if Input.is_action_pressed("ui_select") && mouse_pos.y > ((2/3) * get_viewport_rect().size.y):
+	act_pos = (get_viewport().get_mouse_position() - self.position)
+	charS_act = act_pos.y / 100
+	
+	#t+=1
+	#if t == 60:
+	#	print(charS_act)
+	#	t=0
+	
+	
+	if Input.is_action_pressed("ui_select") && charS_act < 3:
 		_animated_sprite.play("fly")
 		
-	elif Input.is_action_pressed("ui_select") && mouse_pos.y < ((1/3) * get_viewport_rect().size.y):
+	elif Input.is_action_pressed("ui_select") && charS_act > 6:
 		_animated_sprite.play("slide")
 		
-	elif Input.is_action_pressed("ui_select") && mouse_pos.y > ((2/3) * get_viewport_rect().size.y) && mouse_pos.y < ((1/3) * get_viewport_rect().size.y):
+	elif Input.is_action_pressed("ui_select") && charS_act < 6 && charS_act > 3 :
 		_animated_sprite.play("cover")
 		
 	else:
